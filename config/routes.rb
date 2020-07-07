6568afc015992c7f262c20ec8fddc9235a53cb1e
Fizvlad <fizvlad@mail.ru>
2020-07-07 19:34:42 +0300

    Add route to projects#create

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  #get '/projects/', to: 'projects#index'
-  resources 'projects', only: %i[index]
+  get '/projects', to: 'projects#index'
+  post '/projects', to: 'projects#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
