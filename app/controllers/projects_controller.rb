3be7614c64af482acfdb43d9ee0f13e4fad4c14b
Fizvlad <fizvlad@mail.ru>
2020-07-07 19:34:11 +0300

    Add projects#create action

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,9 +1,22 @@
 class ProjectsController < ApplicationController
+  def create
+    project = Project.new(project_params)
+    if project.save
+      render plain: '', status: :created
+    else
+      render plain: project.errors.full_messages.join('; '), status: :unprocessable_entity
+    end
+  end
+
   def index
     render json: projects_json, status: :ok
   end
 
   private
+
+  def project_params
+    params.permit(:title)
+  end
 
   def projects_json
     Project.all.to_json(
