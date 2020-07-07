8afd82b68973c04a9da9f7e4c6ebf0c516a7135e
Fizvlad <fizvlad@mail.ru>
2020-07-07 18:34:40 +0300

    Correct JSON representation of projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,18 @@
 class ProjectsController < ApplicationController
   def index
-    render json: Project.all, status: :ok
+    render json: projects_json, status: :ok
+  end
+
+  private
+
+  def projects_json
+    Project.all.to_json(
+      include: {
+        todos: {
+          except: %i[project_id created_at updated_at]
+        }
+      },
+      except: %i[created_at updated_at]
+    )
   end
 end
