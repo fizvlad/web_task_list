5ca1e8d996e3c8c173fb32a217443d5c3bac5ee7
Fizvlad <fizvlad@mail.ru>
2020-07-07 20:18:48 +0300

    Add todos controller, routes and **yet** empty test file

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,25 @@
+class TodosController < ApplicationController
+  def create
+    todo = Todo.new(todo_params)
+    if todo.save
+      render plain: '', status: :created, location: "/todos/#{todo.id}"
+    else
+      render plain: todo.errors.full_messages.join('; '), status: :unprocessable_entity
+    end
+  end
+
+  def update
+    todo = Todo.find(params[:id])
+    if todo.update(todo_params)
+      render json: todo.to_json(except: %i[created_at updated_at]), status: :ok
+    else
+      render plain: todo.errors.full_messages.join('; '), status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def todo_params
+    params.permit(:text, :isCompleted, :project_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get '/projects', to: 'projects#index'
   post '/projects', to: 'projects#create'
+
+  post '/todos', to: 'todos#create'
+  patch '/projects/:project_id/todo/:id', to: 'todos#update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
