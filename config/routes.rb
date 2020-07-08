f89b0aca337fdb9b84329857abdc6386d2cbfa06
Fizvlad <fizvlad@mail.ru>
2020-07-08 16:19:08 +0300

    Routes naming

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get '/projects', to: 'projects#index'
+  get '/projects', to: 'projects#index', as: 'projects'
   post '/projects', to: 'projects#create'
 
-  post '/todos', to: 'todos#create'
-  patch '/projects/:project_id/todo/:id', to: 'todos#update'
+  post '/todos', to: 'todos#create', as: 'todos'
+  patch '/projects/:project_id/todo/:id', to: 'todos#update', as: 'todo'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
